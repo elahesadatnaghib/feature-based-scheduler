240cc09af97b3259e70019c5aca64ae54cc91b21
Elahesadat naghib <elahesadatnaghib@gmail.com>
2016-10-24 23:56:16 -0400

    reshape 8 by 7 matrix to 1 by 56 vector of parameters

--- Training.py
+++ Training.py
@@ -11,14 +11,14 @@ import time
 
 
 
-Date            = ephem.Date('2016/09/01 12:00:00.00') # times are in UT
+Date            = ephem.Date('2016/11/01 12:00:00.00') # times are in UT
 Site            = ephem.Observer()
 Site.lon        = -1.2320792
 Site.lat        = -0.517781017
 Site.elevation  = 2650
 Site.pressure   = 0.
 Site.horizon    = 0.
-F_weight        = np.array([ 1, 1, 1, 1, 1, 1, 1])
+F_weight        = np.ones([8,7])
 
 preferences     = [1,1,4,0,3,10]
 #P1: cost_av  * -1
@@ -35,7 +35,7 @@ class Training():
         Site.elevation  = 2650
         Site.pressure   = 0.
         Site.horizon    = 0.
-        F_weight        = np.array([ 1, 1, 1, 1, 1, 1, 1])
+        F_weight        = np.ones([8,7])
         DataGen.Night_data(Date, Site)
         self.scheduler = FBDE.Scheduler(Date, Site, F_weight, preferences)
 
@@ -45,21 +45,28 @@ class Training():
         self.optimizer       = MyDE.DE_optimizer(self, N_p, F, Cr, maxIter)
 
     def target(self, x):
-        self.scheduler.set_f_wight(x)
+        temp = np.reshape(x,[8,7])
+        self.scheduler.set_f_wight(temp)
         self.scheduler.schedule()
-        return -1*self.scheduler.performance()
+        return -1 * self.scheduler.performance()
 
 
 s       = time.time()
 train   = Training(Date, Site, preferences)
 
-N_p     = 100
+N_p     = 90
 F       = 0.8
 Cr      = 0.8
 maxIter = 20
-Domain  = np.array([[0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10]])
-D       = 7
+Domain  = np.array([[0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10],
+                    [0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10],
+                    [0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10],
+                    [0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10],
+                    [0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10],
+                    [0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10],
+                    [0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10],
+                    [0,10], [0,10], [0,10], [0,10], [0,10], [0,10], [0,10]])
+D       = 56
 train.DE_opt(N_p, F, Cr, maxIter, D, Domain)
 
 print('Total elapsed time: {} sec'.format(time.time() - s))
-
