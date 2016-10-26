584b2b4d29c575480fef2b18aa9f63c949f89a53
Elahesadat naghib <elahesadatnaghib@gmail.com>
2016-10-26 17:03:11 -0400

    Prepared for a year of simulation

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ import NightDataGenerator as DataGen
 import FBDE
 import time
 import DBreadwrite as DB
+import Graphics as GP
 import os
 
 
@@ -17,7 +18,7 @@ Site.elevation  = 2650
 Site.pressure   = 0.
 Site.horizon    = 0.
 
-preferences     = [1,1,10,2,3,10] # mission objective
+preferences     = [1, 1, 4, 0, 3, 10] # mission objective
 # objective function
 #objective = preferences[0] * average cost * -1 +
 #            preferences[1] * average slew time * -1 +
@@ -28,16 +29,16 @@ preferences     = [1,1,10,2,3,10] # mission objective
 
 #F_weight : controller parameters
 #F_weight        = np.ones([8,7])
-F_weight        = np.reshape(np.array([3.82825499,  5.0246024,   0.39586326,  5.22392334,  5.69871546,  1.07005201,
-                            2.80493447,  9.76303104,  6.21157402,  3.26728548,  5.2239269,   1.61210018,
-                            1.90632446,  2.35685668,  0.76146984,  8.41668302,  3.18580931,  4.04224808,
-                            5.31969773,  9.16275397,  5.17652933,  6.11968075,  8.70230757,  6.42902741,
-                            2.67848656,  2.71030438,  8.39292199,  8.24898974,  0.48159622,  5.79235008,
-                            2.82793412,  5.21648905,  3.62208473,  1.71775652,  3.3032883,   7.32164841,
-                            1.49429437,  3.74921596,  5.67211172,  0.48162585,  8.88405316,  0.53897842,
-                            5.39172567,  2.48267169,  7.946164,    9.02791093,  0.56115775,  2.45772551,
-                            8.26570773,  6.0443104,   1.6260163,   1.03051747,  2.82285864,  7.45984642,
-                            3.36747846,  8.52111997]),[8,7])  # learning result
+F_weight        = np.reshape(np.array([ 4.20719114,  7.30495229,  3.22797504,  7.36876567,  2.48813029,  2.25788253,
+                                        2.06713858,  0.6136642,   4.13515909,  1.91470662,  8.47890309,  4.68662087,
+                                        8.29988236,  4.70413934,  2.78585885,  6.60923852,  3.94503914,  5.62561784,
+                                        4.01397898,  5.59309747,  4.00137767,  7.85457785,  7.9947913,   2.00940157,
+                                        6.40087058,  5.27213064,  5.41139189,  5.7581916,   6.52381396,  5.79545407,
+                                        6.7566571,   4.81852891,  4.79779777,  4.4556204,   1.43700385,  6.70733977,
+                                        2.47125883,  4.53605309,  5.26014559,  8.0306582,   4.56409016,  4.13744006,
+                                        1.96368802,  4.61564592,  4.25298633,  3.56414258,  4.92874663,  5.87943215,
+                                        9.32594932,  1.39163788,  9.16930444,  3.62697418,  3.18903897,  4.23514478,
+                                        7.15683848,  6.20172087]),[8,7])  # learning result
 
 # F1: slew time cost 0~2
 # F2: night urgency -1~1
@@ -50,7 +51,7 @@ F_weight        = np.reshape(np.array([3.82825499,  5.0246024,   0.39586326,  5.
 # immediate reward reward = F_weight[0] * F1 + F_weight[1] * F2 + F_weight[2] * F3 + F_weight[3] * F4 + F_weight[4] * F5 + F_weight[5] * F6 + F_weight[6] * F7
 s = time.time()
 
-n_nights = 1 # number of the nights to be scheduled starting from 1st Sep. 2016
+n_nights = 365 # number of the nights to be scheduled starting from 1st Sep. 2016
 
 
 # Delete previous database
@@ -59,7 +60,7 @@ try:
 except:
     pass
 
-for i in range(10, n_nights+1):
+for i in range(1, n_nights+1):
     Date = ephem.Date('2016/09/{} 12:00:00.00'.format(i)) # times are in UT
     # Delete previous history dependent data
     try:
@@ -88,7 +89,13 @@ for i in range(10, n_nights+1):
     t4 = time.time()
     print('\nWriting on DB finished in {} sec'.format(t4 - t3))
 
+    # Animation specifications
+    FPS = 10            # Frame per second
+    Steps = 100          # Simulation steps
+    MP4_quality = 300   # MP4 size and quality
 
+    PlotID = 2        # 1 for one Plot, 2 for including covering pattern
+    GP.Visualize(Date, PlotID ,FPS, Steps, MP4_quality, 'Visualizations/LSST1plot{}.mp4'.format(int(ephem.julian_date(Date))), showClouds= False)
 
 print('\n \nTotal elapsed time: {} sec'.format(time.time() - s))
 
